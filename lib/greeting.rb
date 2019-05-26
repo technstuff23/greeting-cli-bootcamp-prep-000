90491a08371ccad1c4ca9931b09e1fd850beeda7
K Branch <dancingal212@gmail.com>
2019-05-26 20:11:44 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-puts "Hello #{name}. It's nice to meet you."
+puts "Hello #{name.strip}. It's nice to meet you."
 end
 
 #puts "Oh Nah, Nah. What's your name?"
